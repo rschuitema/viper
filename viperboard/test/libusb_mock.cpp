f992ab86d0f4e97ffec9528bbd45fddff7f73445
Robbert Schuitema <robbert.schuitema@gmail.com>
2017-12-25 10:22:31 +0100

    added mock for libusb

---
+++ viperboard/test/libusb_mock.cpp
@@ -0,0 +1,157 @@
+#include "libusb_mock.hpp"
+
+LibUsbMock* pLibUsbMock;
+
+extern "C" {
+
+   STATUS_T libusb_init(libusb_context **ctx)
+   {
+       return pLibUsbMock->init(ctx);
+   }
+
+   void libusb_exit(libusb_context *ctx)
+   {
+       pLibUsbMock->exit(ctx);
+   }
+
+   const char * libusb_error_name(int errcode)
+   {
+        return pLibUsbMock->error_name(errcode);
+   }
+
+   const struct libusb_version* libusb_get_version()
+   {
+        return pLibUsbMock->get_version();
+   }
+
+   int libusb_has_capability(uint32_t capability)
+   {
+        return pLibUsbMock->has_capability(capability);
+   }
+
+   int libusb_hotplug_register_callback(libusb_context *ctx,
+                                        libusb_hotplug_event events,
+                                        libusb_hotplug_flag flags,
+                                        int vendor_id, int product_id,
+                                        int dev_class,
+                                        libusb_hotplug_callback_fn cb_fn,
+                                        void *user_data,
+                                        libusb_hotplug_callback_handle *handle)
+   {
+       return pLibUsbMock->hotplug_register_callback(ctx, events, flags,
+                                                     vendor_id, product_id, dev_class,
+                                                      cb_fn, user_data, handle);
+   }
+   ssize_t libusb_get_device_list(libusb_context *ctx, libusb_device ***list)
+   {
+       return pLibUsbMock->get_device_list(ctx, list);
+   }
+
+   int libusb_get_device_descriptor(libusb_device *dev, struct libusb_device_descriptor *desc)
+   {
+        return pLibUsbMock->get_device_descriptor(dev, desc);
+   }
+
+   int libusb_get_device_speed(libusb_device *dev)
+   {
+        return pLibUsbMock->get_device_speed(dev);
+   }
+
+   uint8_t libusb_get_bus_number(libusb_device *dev)
+   {
+        return pLibUsbMock->get_bus_number(dev);
+   }
+   uint8_t libusb_get_port_number(libusb_device *dev)
+   {
+        return pLibUsbMock->get_port_number(dev);
+   }
+   int libusb_get_port_numbers(libusb_device *dev, uint8_t* port_numbers, int port_numbers_len)
+   {
+        return pLibUsbMock->get_port_numbers(dev, port_numbers, port_numbers_len);
+   }
+
+   int libusb_open(libusb_device *dev, libusb_device_handle **handle)
+   {
+        return pLibUsbMock->open(dev, handle);
+   }
+
+   void libusb_close(libusb_device_handle *handle)
+   {
+        pLibUsbMock->close(handle);
+   }
+
+   int libusb_get_string_descriptor_ascii(libusb_device_handle *dev, uint8_t desc_index, unsigned char *data, int length)
+   {
+        return pLibUsbMock->get_string_descriptor_ascii(dev, desc_index, data, length);
+   }
+   void libusb_set_debug(libusb_context* ctx, int level)
+   {
+        return pLibUsbMock->set_debug(ctx, level);
+   }
+
+   int libusb_handle_events_timeout_completed(libusb_context *ctx, struct timeval *tv, int *completed)
+   {
+        return pLibUsbMock->handle_events_timeout_completed(ctx, tv, completed);
+   }
+
+   void libusb_free_device_list(libusb_device **list, int unref_devices) 
+   {
+        return pLibUsbMock->free_device_list(list, unref_devices);
+   }
+
+
+   int libusb_control_transfer ( libusb_device_handle *  dev_handle,
+                                 uint8_t     bmRequestType,
+                                 uint8_t     bRequest,
+                                 uint16_t    wValue,
+                                 uint16_t    wIndex,
+                                 unsigned char *     data,
+                                 uint16_t    wLength,
+                                 unsigned int    timeout)
+   {
+        return pLibUsbMock->control_transfer(dev_handle, bmRequestType, bRequest, wValue, wIndex, data, wLength, timeout);
+   }
+
+    int libusb_bulk_transfer(struct libusb_device_handle *   dev_handle,
+                             unsigned char   endpoint,
+                             unsigned char * data,
+                             int             length,
+                             int *           transferred,
+                             unsigned int    timeout)
+    {
+        return pLibUsbMock->bulk_transfer(dev_handle, endpoint, data, length, transferred, timeout);
+    }
+
+    int libusb_get_active_config_descriptor(libusb_device *dev,
+         struct libusb_config_descriptor **config)
+    {
+        return pLibUsbMock->get_active_config_descriptor(dev, config);
+    }
+
+    void libusb_free_config_descriptor(struct libusb_config_descriptor *config)
+    {
+        return pLibUsbMock->free_config_descriptor(config);
+    }
+
+    int libusb_claim_interface(libusb_device_handle *dev, int interface_number)
+    {
+        return pLibUsbMock->claim_interface(dev, interface_number);
+    }
+    int libusb_release_interface(libusb_device_handle *dev, int interface_number)
+    {
+        return pLibUsbMock->release_interface(dev, interface_number);
+    }
+
+    int libusb_clear_halt(libusb_device_handle *dev, unsigned char endpoint)
+    {
+        return pLibUsbMock->clear_halt(dev, endpoint);
+    }
+    int libusb_reset_device(libusb_device_handle *dev)
+    {
+        return pLibUsbMock->reset_device(dev);
+    }
+    int libusb_get_configuration(libusb_device_handle *dev, int *config)
+    {
+        return pLibUsbMock->get_configuration(dev, config);
+    }
+}
---
+++ viperboard/test/libusb_mock.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <stdint.h>
+#include <libusb.h>
+#include "gmock/gmock.h"
+
+#include "BoostTestAdapter.hpp"
+
+typedef int32_t STATUS_T;
+
+/*
+    Since lib usb device is a opaque data type provide some definition.
+*/
+struct libusb_device {
+    int id;
+};
+
+struct libusb_device_handle {
+    int id;
+};
+struct libusb_context {
+    int id;
+};
+
+class LibUsbMock
+{
+public:
+    MOCK_METHOD1(init, STATUS_T(libusb_context**));
+    MOCK_METHOD1(exit, void(libusb_context*));
+    MOCK_METHOD0(get_version, const struct libusb_version*());
+
+    MOCK_METHOD2(set_debug, void(libusb_context*, int));
+
+    MOCK_METHOD1(error_name, const char * (int));
+    MOCK_METHOD1(has_capability, int(uint32_t));
+
+    MOCK_METHOD9(hotplug_register_callback, int(libusb_context*,
+                                                libusb_hotplug_event,
+                                                libusb_hotplug_flag,
+                                                int, int, int,
+                                                libusb_hotplug_callback_fn,
+                                                void*,
+                                                libusb_hotplug_callback_handle*));
+
+    MOCK_METHOD2(get_device_list, ssize_t(libusb_context* , libusb_device***));
+    MOCK_METHOD2(get_device_descriptor, int(libusb_device*, struct libusb_device_descriptor*));
+    MOCK_METHOD1(get_device_speed, int(libusb_device*));
+    MOCK_METHOD1(get_bus_number, uint8_t(libusb_device*));
+    MOCK_METHOD1(get_port_number, uint8_t(libusb_device*));
+    MOCK_METHOD3(get_port_numbers, int(libusb_device*, uint8_t*, int));
+
+    MOCK_METHOD2(open, int(libusb_device *dev, libusb_device_handle **handle));
+    MOCK_METHOD1(close, void(libusb_device_handle *handle));
+    MOCK_METHOD4(get_string_descriptor_ascii, int(libusb_device_handle *dev, uint8_t desc_index, unsigned char *data, int length));
+
+    MOCK_METHOD3(handle_events_timeout_completed, int(libusb_context*, struct timeval*, int*));
+    MOCK_METHOD2(free_device_list, void(libusb_device**,int));
+
+    MOCK_METHOD8(control_transfer, int( libusb_device_handle *  dev_handle,
+                                        uint8_t     bmRequestType,
+                                        uint8_t     bRequest,
+                                        uint16_t    wValue,
+                                        uint16_t    wIndex,
+                                        unsigned char *     data,
+                                        uint16_t    wLength,
+                                        unsigned int    timeout));
+
+    MOCK_METHOD6(bulk_transfer, int (struct libusb_device_handle *   dev_handle,
+                                     unsigned char   endpoint,
+                                     unsigned char * data,
+                                     int             length,
+                                     int *           transferred,
+                                     unsigned int    timeout));
+
+    MOCK_METHOD2(get_active_config_descriptor, int(libusb_device *dev,
+                                                   struct libusb_config_descriptor **config));
+    MOCK_METHOD1(free_config_descriptor, void(struct libusb_config_descriptor *config));
+
+    MOCK_METHOD2(claim_interface, int(libusb_device_handle *dev, int interface_number));
+    MOCK_METHOD2(release_interface, int(libusb_device_handle *dev, int interface_number));
+
+    MOCK_METHOD2(clear_halt, int(libusb_device_handle *dev, unsigned char endpoint));
+    MOCK_METHOD1(reset_device, int(libusb_device_handle *dev));
+
+    MOCK_METHOD2(get_configuration, int(libusb_device_handle *dev, int *config));
+};
+
+
+extern LibUsbMock* pLibUsbMock;
+
+/* Boost requires a simple class to construct, since we use Gmock to wrap a C interface
+we need to use pLibUsbMock. This simple struct just wraps to this specific pointer */
+struct BoostLibUsbMock {
+    BoostLibUsbMock()
+    {
+         pLibUsbMock = new LibUsbMock();
+    }
+    ~BoostLibUsbMock()
+    {
+        delete pLibUsbMock;
+    }
+};
