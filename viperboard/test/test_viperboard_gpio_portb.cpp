50dfd416ba301f455072a4f96c3d9dbf3f8fe143
Robbert Schuitema <robbert.schuitema@gmail.com>
2018-01-03 19:46:46 +0100

    add testcases for setting direction of gpio port b

--- viperboard/test/test_viperboard_gpio_portb.cpp
+++ viperboard/test/test_viperboard_gpio_portb.cpp
@@ -12,6 +12,8 @@ using ::testing::Throw;
 using ::testing::Eq;
 using ::testing::A;
 using ::testing::SaveArgPointee;
+using ::testing::WithArg;
+using ::testing::DoAll;
 
 using namespace Viper;
 
@@ -30,7 +32,7 @@ class ViperboardGpioBTest : public ::testing::Test
         {
             pLibUsbMock = new LibUsbMock();
 
-            EXPECT_CALL(*pLibUsbMock, init(_)).WillOnce(DoAll(SetArgPointee<0>(&context), Return(LIBUSB_SUCCESS)));
+            EXPECT_CALL(*pLibUsbMock, init(_)).WillOnce( DoAll( SetArgPointee<0>(&context), Return(LIBUSB_SUCCESS) ) );
             pViper = new Viperboard();
 
             EXPECT_FALSE(nullptr == pViper);
@@ -55,6 +57,7 @@ class ViperboardGpioBTest : public ::testing::Test
             delete pViper;
             delete pLibUsbMock;
         }
+        
 };
 
 // The testcases can asume the viperboard is opened and closed successfully in the setup and teardown
@@ -78,15 +81,72 @@ TEST_F(ViperboardGpioBTest, GetGpioInterfaceTwiceReturnsSameInterface)
    
 }
 
+ACTION_P2(SaveArrayPointee, pointer, length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        pointer[i] = arg0[i];
+    }
+}
+
+
 TEST_F(ViperboardGpioBTest, SetPortAllBitsInputSuccess)
 {
     ViperResult_t result = VIPER_OTHER_ERROR;
     IGPIO_PortB* pGpio = pViper->GetGpioPortBInterface();
+    uint8_t data[50] = {0xAA};
+    int16_t length = 5;
+
+    
+    EXPECT_CALL(*pLibUsbMock, control_transfer(_, Eq(0x40), Eq(0xDD), Eq(0x0000), Eq(0x0000), _, Eq(5u), Eq(1000u))).WillOnce( DoAll( WithArg<5>(SaveArrayPointee(data, length)), Return(5) ) );
     
     result = pGpio->SetPortDirection(0x0000, 0xFFFF);
     
+    ASSERT_EQ(0x00, data[0]);
+    ASSERT_EQ(0x00, data[1]);
+    ASSERT_EQ(0x00, data[2]);
+    ASSERT_EQ(0xFF, data[3]);
+    ASSERT_EQ(0xFF, data[4]);
     ASSERT_EQ(VIPER_SUCCESS, result);
 }
 
+TEST_F(ViperboardGpioBTest, SetPortSomeBitsInputSuccess)
+{
+    ViperResult_t result = VIPER_OTHER_ERROR;
+    IGPIO_PortB* pGpio = pViper->GetGpioPortBInterface();
+    uint8_t data[50] = {0xAA};
+    int16_t length = 5;
 
+    
+    EXPECT_CALL(*pLibUsbMock, control_transfer(_, Eq(0x40), Eq(0xDD), Eq(0x0000), Eq(0x0000), _, Eq(5u), Eq(1000u))).WillOnce( DoAll( WithArg<5>(SaveArrayPointee(data, length)), Return(5) ) );
+    
+    result = pGpio->SetPortDirection(0x34AE, 0xFFFF);
+    
+    ASSERT_EQ(0x00, data[0]);
+    ASSERT_EQ(0x34, data[1]);
+    ASSERT_EQ(0xAE, data[2]);
+    ASSERT_EQ(0xFF, data[3]);
+    ASSERT_EQ(0xFF, data[4]);
+    ASSERT_EQ(VIPER_SUCCESS, result);
+}
+
+TEST_F(ViperboardGpioBTest, SetPortOnlyAllowedBitsInputSuccess)
+{
+    ViperResult_t result = VIPER_OTHER_ERROR;
+    IGPIO_PortB* pGpio = pViper->GetGpioPortBInterface();
+    uint8_t data[50] = {0xAA};
+    int16_t length = 5;
+
+    
+    EXPECT_CALL(*pLibUsbMock, control_transfer(_, Eq(0x40), Eq(0xDD), Eq(0x0000), Eq(0x0000), _, Eq(5u), Eq(1000u))).WillOnce( DoAll( WithArg<5>(SaveArrayPointee(data, length)), Return(5) ) );
+    
+    result = pGpio->SetPortDirection(0x0000, 0xF05A);
+    
+    ASSERT_EQ(0x00, data[0]);
+    ASSERT_EQ(0x00, data[1]);
+    ASSERT_EQ(0x00, data[2]);
+    ASSERT_EQ(0xF0, data[3]);
+    ASSERT_EQ(0x5A, data[4]);
+    ASSERT_EQ(VIPER_SUCCESS, result);
+}
 
