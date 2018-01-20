0f2f9d55d791f679ee397827c08171d6b00465a1
Robbert Schuitema <robbert.schuitema@gmail.com>
2018-01-20 08:43:03 +0100

    added testcase that check is transaction failure is returned when libusb returns an error

--- viperboard/test/test_viperboard_gpio_porta.cpp
+++ viperboard/test/test_viperboard_gpio_porta.cpp
@@ -146,3 +146,31 @@ TEST_F(ViperboardGpioATest, SetPWMModeIncorrectNrBytesTransactionfailure)
     ASSERT_EQ(0x00, data[10]);
     ASSERT_EQ(VIPER_TRANSACTION_FAILURE, result);
 }
+
+TEST_F(ViperboardGpioATest, SetPWMModeLibusbErrorTransactionfailure)
+{
+    ViperResult_t result = VIPER_OTHER_ERROR;
+    IGPIO_PortA* pGpio = pViper->GetGpioPortAInterface();
+    uint8_t bit = 4;
+    uint8_t level = 55;
+    uint8_t clock = 0x73;
+    uint16_t length = 11;
+    uint8_t data[50] = {0xAA};
+
+    EXPECT_CALL(*pLibUsbMock, control_transfer(_, Eq(0x40), Eq(0xED), Eq(0x0000), Eq(0x0000), _, Eq(length), Eq(1000u))).WillOnce(DoAll(WithArg<5>(SaveArrayPointee(data, length)), Return(LIBUSB_ERROR_NO_DEVICE)));
+
+    result = pGpio->SetPWMMode(bit, level, clock);
+
+    ASSERT_EQ(0x02, data[0]);
+    ASSERT_EQ(clock, data[1]);
+    ASSERT_EQ(bit, data[2]);
+    ASSERT_EQ(0x00, data[3]);
+    ASSERT_EQ(0x00, data[4]);
+    ASSERT_EQ(0x00, data[5]);
+    ASSERT_EQ(level, data[6]);
+    ASSERT_EQ(0x00, data[7]);
+    ASSERT_EQ(0x00, data[8]);
+    ASSERT_EQ(0x00, data[9]);
+    ASSERT_EQ(0x00, data[10]);
+    ASSERT_EQ(VIPER_TRANSACTION_FAILURE, result);
+}
