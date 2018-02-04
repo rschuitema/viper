5004dcc7bb3e27d01aa7d9f422965df33c0319c2
Robbert Schuitema <robbert.schuitema@gmail.com>
2018-02-04 22:33:26 +0100

    added failing test, getdigitalinput should return transaction failure when second call to libusb returns incorrect number of bytes

--- viperboard/test/test_viperboard_gpio_porta.cpp
+++ viperboard/test/test_viperboard_gpio_porta.cpp
@@ -723,3 +723,33 @@ TEST_F(ViperboardGpioATest, GetDigitalInputIncorrecNrBytesFirstCallTransactionFa
     ASSERT_EQ(VIPER_TRANSACTION_FAILURE, result);
     ASSERT_FALSE(value);
 }
+
+TEST_F(ViperboardGpioATest, GetDigitalInputIncorrecNrBytesSecondCallTransactionFailure)
+{
+    ViperResult_t result = VIPER_OTHER_ERROR;
+    IGPIO_PortA* pGpio = pViper->GetGpioPortAInterface();
+    uint8_t bit = 8;
+    uint16_t length = 11;
+    bool value = true;
+    uint8_t data[50] = {0xAA};
+    uint8_t returndata[11] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+    EXPECT_CALL(*pLibUsbMock, control_transfer(_, Eq(0x40), Eq(0xED), Eq(0x0000), Eq(0x0000), _, Eq(length), Eq(1000u))).WillOnce(DoAll(WithArg<5>(SaveArrayPointee(data, length)), Return(length)));
+    EXPECT_CALL(*pLibUsbMock, control_transfer(_, Eq(0xC0), Eq(0xED), Eq(0x0000), Eq(0x0000), _, Eq(length), Eq(1000u))).WillOnce(DoAll(SetArrayArgument<5>(returndata, returndata+length), Return(1)));
+
+    result = pGpio->GetDigitalInput(bit, &value);
+
+    ASSERT_EQ(0x06, data[0]);
+    ASSERT_EQ(0x00, data[1]);
+    ASSERT_EQ(bit, data[2]);
+    ASSERT_EQ(0x00, data[3]);
+    ASSERT_EQ(0x00, data[4]);
+    ASSERT_EQ(0x00, data[5]);
+    ASSERT_EQ(0x00, data[6]);
+    ASSERT_EQ(0x00, data[7]);
+    ASSERT_EQ(0x00, data[8]);
+    ASSERT_EQ(0x00, data[9]);
+    ASSERT_EQ(0x00, data[10]);
+    ASSERT_EQ(VIPER_TRANSACTION_FAILURE, result);
+    ASSERT_FALSE(value);
+}
