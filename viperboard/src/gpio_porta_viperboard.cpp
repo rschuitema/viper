60af0d05dba374f85a12129c01fa07b3e993ee0e
Robbert Schuitema <robbert.schuitema@gmail.com>
2018-05-10 10:16:41 +0200

    setinterruptinputmode now returns invalid parameter when called with incorrect bit

--- viperboard/src/gpio_porta_viperboard.cpp
+++ viperboard/src/gpio_porta_viperboard.cpp
@@ -220,6 +220,11 @@ namespace Viper
             int bytes_transferred = 0;
 	    uint8_t buffer[11];
 
+	    if (bit >15)
+	    {
+	        return VIPER_INVALID_PARAMETER;
+	    }
+    
 	    buffer[0] = 0x05;
 	    buffer[1] = clock;
 	    buffer[2] = bit;
